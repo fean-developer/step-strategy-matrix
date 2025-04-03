bbbc74256fa65a83a17d1d9171d7a99175a7c50b
Francisco Nascimento <francisco.nascimento@bradesco.com.br>
2025-04-02 23:54:13 -0300

    testando matix

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,9 @@ async function run() {
       throw new Error("O input 'matrix' deve ser um array JSON.");
     }
 
-    core.info(`Executando para ambientes: ${matrix.join(", ")}`);
-
     // Itera sobre os ambientes e executa o comando para cada um
     for (const env of matrix) {
       const finalCommand = command.replace(/\$AMBIENTE/g, env);
-
-      core.info(`Executando comando para: ${env}`);
-      core.info(`Comando: ${finalCommand}`);
 
       await new Promise<void>((resolve, reject) => {
         exec(finalCommand, (error, stdout, stderr) => {
