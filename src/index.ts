f1c805d0b894d227a57bb296a0db68eb45e5cf85
Francisco Nascimento <francisco.nascimento@bradesco.com.br>
2025-04-03 10:40:45 -0300

    Testando output

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as core from "@actions/core";
 import { exec } from "child_process";
 
 async function run() {
+  try {
     // Obtém os inputs
     const matrixInput = core.getInput("matrix", { required: true });
     const command = core.getInput("command", { required: true });
@@ -16,19 +17,31 @@ async function run() {
     // Itera sobre os ambientes e executa o comando para cada um
     for (const env of matrix) {
       const finalCommand = command.replace(/\${environment}/g, env);
-
-      await new Promise<void>((resolve) => {
-        exec(finalCommand, ( stdout) => {
-        //   if (error) {
-        //     core.error(`Erro ao executar ${env}: ${stderr}`);
-        //     reject(error);
-        //     return;
-        //   }
-          core.info(`${stdout}`);
+    
+      await new Promise<void>((resolve, reject) => {
+        exec(finalCommand, (error, stdout, stderr) => {
+          // Exibe a saída padrão (stdout) e a saída de erro (stderr)
+          if (stdout) {
+            core.info(`Saída do comando (${env}):\n${stdout}`);
+          }
+          if (stderr) {
+            core.error(`Erro do comando (${env}):\n${stderr}`);
+          }
+    
+          if (error) {
+            // Rejeita a Promise com o erro, mas já exibiu as saídas
+            reject(new Error(`Erro ao executar o comando para o ambiente ${env}: ${error.message}`));
+            return;
+          }
+    
           resolve();
         });
       });
     }
+
+  } catch (error: any) {
+    core.setFailed(`Erro: ${error.message}`);
+  }
 }
 
 run();
