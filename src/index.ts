f327996425a30bdde8359878c9055e1c4f66c5dc
Francisco Nascimento <francisco.nascimento@bradesco.com.br>
2025-04-03 10:50:27 -0300

    Testando output

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as core from "@actions/core";
 import { exec } from "child_process";
 
 async function run() {
+  try {
     // Obtém os inputs
     const matrixInput = core.getInput("matrix", { required: true });
     const command = core.getInput("command", { required: true });
@@ -16,7 +17,7 @@ async function run() {
     // Itera sobre os ambientes e executa o comando para cada um
     for (const env of matrix) {
       const finalCommand = command.replace(/\${environment}/g, env);
-    
+
       await new Promise<void>((resolve, reject) => {
         exec(finalCommand, (error, stdout, stderr) => {
           // Exibe a saída padrão (stdout) e a saída de erro (stderr)
@@ -26,18 +27,21 @@ async function run() {
           if (stderr) {
             core.error(`Erro do comando (${env}):\n${stderr}`);
           }
-    
+
           if (error) {
-            // Rejeita a Promise com uma mensagem personalizada, sem incluir o comando
-            reject();
+            // Rejeita a Promise com uma mensagem personalizada
+            reject(new Error(`Erro ao executar o comando para o ambiente ${env}: ${stderr}`));
             return;
           }
-    
+
           resolve();
         });
       });
     }
-  
+  } catch (error) {
+    // Captura e exibe o erro
+    core.setFailed(`Erro na execução: ${error instanceof Error ? error.message : error}`);
+  }
 }
 
 run();
