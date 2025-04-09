24e1e36d817c8f94a8b645973be4918883c88b11
Francisco Nascimento <francisco.nascimento@bradesco.com.br>
2025-04-09 13:21:56 -0300

    fix: Squashing todos os commits

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import * as core from "@actions/core";
+import { exec } from "child_process";
+
+async function run() {
+  try {
+    // Obtém os inputs
+    const matrixInput = core.getInput("matrix", { required: true });
+    const command = core.getInput("command", { required: true });
+
+    // Converte JSON para array
+    const matrix: string[] = JSON.parse(matrixInput);
+
+    if (!Array.isArray(matrix)) {
+      throw new Error("O input 'matrix' deve ser um array JSON.");
+    }
+
+    // Armazena os erros encontrados
+    const errors: string[] = [];
+
+    // Itera sobre os ambientes e executa o comando para cada um
+    for (const env of matrix) {
+      const finalCommand = command.replace(/\{\{\ matrix.environment \}\}/g, env);
+
+      await new Promise<void>((resolve) => {
+        exec(finalCommand, (error, stdout, stderr) => {
+          // Exibe a saída padrão (stdout) e a saída de erro (stderr)
+          if (stdout) {
+            core.info(`Executando matrix[${env}]:\n${stdout}`);
+          }
+          if (stderr) {
+            core.error(`Erro do comando (${env}):\n${stderr}`);
+          }
+
+          if (error) {
+            // Armazena o erro, mas não interrompe a execução
+            errors.push(env);
+          }
+
+          resolve();
+        });
+      });
+    }
+
+    // Lança os erros acumulados, se houver
+    if (errors.length > 0) {
+      throw new Error(`Foram encontrados erros durante a execução: ${errors.join("|")}`);
+    }
+  } catch (error) {
+    // Captura e exibe o erro
+    core.setFailed(`${error instanceof Error ? error.message : 'Erros encontrado em ' + error}`);
+  }
+}
+
+run();
